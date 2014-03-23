e59ccd935d31bdeb92e06d0867374a12a6253a94
Camlorn <camlorn38@gmail.com>
2014-03-23 01:10:34 -0400

    Removed pycppreflect and everything related to it.  Pycppreflect has too many limitations.

--- pycppreflect/filters/filterbase.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-class FilterBase(object):
-	def __init__(self):
-		self.results = None
-		
-	def filter(self,objects):
-		raise NotImplementedError( "filter not implemented in class" )
-		
