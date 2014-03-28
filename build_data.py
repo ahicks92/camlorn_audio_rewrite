1d7d790e903c3aaf653a9de84b8f476f1ae89d83
Camlorn <camlorn38@gmail.com>
2014-03-27 20:21:50 -0400

    The data extraction step is now separated from the step that writes the yml files.

--- build_data.py
+++ build_data.py
@@ -1,6 +1,7 @@
 """A script that builds all the information from the OpenAL headers."""
 import build_metadata.extract_from_headers
 import os.path
+import yaml
 
 headers =  set([
 os.path.join('OpenAL-Soft', 'include', 'al', 'al.h'),
@@ -12,4 +13,7 @@ for i in headers:
 	headers.remove(i)
 	headers.add(os.path.join(my_directory, i))
 
-build_metadata.extract_from_headers.extract_from_headers(headers, target_directory = os.path.join(my_directory, 'extracted_data'))
+yml_files = build_metadata.extract_from_headers.extract_from_headers(headers, target_directory = os.path.join(my_directory, 'extracted_data'))
+for k, v in yml_files.iteritems():
+	outfile = file(os.path.join(my_directory, 'extracted_data', k+'.yml'), 'w')
+	yaml.dump(data = v, stream = outfile, default_flow_style = False)
--- build_metadata/extract_from_headers.py
+++ build_metadata/extract_from_headers.py
@@ -12,11 +12,4 @@ def extract_from_headers(headers, target_directory):
 		functions[i.name]['name'] = i.name
 		functions[i.name]['return_type'] = i.return_type
 		functions[i.name]['arguments'] = list([list(j) for j in i.arguments]) #makes the output much more readable, at the loss of importing as list instead.
-	macro_file = file(os.path.join(target_directory, 'macros.yml'), 'w')
-	function_file = file(os.path.join('extracted_data', 'functions.yml'), 'w')
-
-	yaml.dump(data = macros, stream = macro_file, default_flow_style = False)
-	yaml.dump(data = functions, stream = function_file)
-
-if __name__ == '__main__':
-	extract_everything()
+	return {'macros': macros, 'functions': functions}
