631e3feb5a513bf034a3c335084aed515ad17ccb
Camlorn <camlorn38@gmail.com>
2014-03-27 20:03:35 -0400

    Files which are computed from OpenAL headers are no longer included in the repository and are placed in a subdirectory.

--- extract_open_al.py
+++ extract_open_al.py
@@ -1,5 +1,7 @@
 import clang_helper
 import yaml
+import os
+import os.path
 
 def extract_everything():
 	everything = clang_helper.FeatureExtractor(['all_open_al.h'])
@@ -10,8 +12,11 @@ def extract_everything():
 		functions[i.name]['name'] = i.name
 		functions[i.name]['return_type'] = i.return_type
 		functions[i.name]['arguments'] = list([list(j) for j in i.arguments]) #makes the output much more readable, at the loss of importing as list instead.
-	macro_file = file('macros.yml', 'w')
-	function_file = file('functions.yml', 'w')
+	if not os.path.isdir('extracted_data'):
+		os.mkdir('extracted_data')
+	macro_file = file('extracted_data/macros.yml', 'w')
+	function_file = file('extracted_data/functions.yml', 'w')
+
 	yaml.dump(data = macros, stream = macro_file, default_flow_style = False)
 	yaml.dump(data = functions, stream = function_file)
 
