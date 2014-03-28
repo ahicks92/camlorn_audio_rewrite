6526e855fa0302957fe403c8181985349245efe3
Camlorn <camlorn38@gmail.com>
2014-03-27 20:28:15 -0400

    Readability improvement to build_data.py output.

--- build_data.py
+++ build_data.py
@@ -14,6 +14,7 @@ for i in headers:
 	headers.add(os.path.join(my_directory, i))
 
 yml_files = build_metadata.extract_from_headers.extract_from_headers(headers, target_directory = os.path.join(my_directory, 'extracted_data'))
-for k, v in yml_files.iteritems():
-	outfile = file(os.path.join(my_directory, 'extracted_data', k+'.yml'), 'w')
-	yaml.dump(data = v, stream = outfile, default_flow_style = False)
+with file(os.path.join(my_directory, 'extracted_data', 'functions.yml'), 'w') as outfile:
+	yaml.dump(data = yml_files['functions'], stream = outfile)
+with file(os.path.join(my_directory, 'extracted_data', 'macros.yml'), 'w') as outfile:
+	yaml.dump(data = yml_files['macros'], stream = outfile, default_flow_style = False)
